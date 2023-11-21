e30ba44141f2442c785288cb892022322fd1acd6
kushal800 <97936111+kushal800@users.noreply.github.com>
2023-11-21 11:08:37 +0530

    13072.py

---
+++ 13072.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def generate_signal(freq, duration, sample_rate):
+    t = np.arange(0, duration, 1/sample_rate)
+    return np.sin(2 * np.pi * freq * t)
+
+def plot_spectrum(signal, sample_rate):
+    n = len(signal)
+    k = np.arange(n)
+    T = n / sample_rate
+    frq = k / T  # two sides frequency range
+    frq = frq[:n // 2]  # one side frequency range
+
+    Y = np.fft.fft(signal) / n  # fft computing and normalization
+    Y = Y[:n // 2]
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(frq, abs(Y))
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Amplitude')
+    plt.title('Frequency Spectrum')
+    plt.show()
+
+def main():
+    sample_rate = 1000  # Hz
+    duration = 1  # seconds
+    freq = 5  # Hz
+
+    signal = generate_signal(freq, duration, sample_rate)
+    plot_spectrum(signal, sample_rate)
+
+if __name__ == "__main__":
+    main()
