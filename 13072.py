583b9e4345326d089cf411c29340a404b733247a
kushal800 <97936111+kushal800@users.noreply.github.com>
2023-11-21 11:25:34 +0530

    13072.py

--- 13072.py
+++ 13072.py
@@ -11,24 +11,23 @@ def plot_spectrum(signal, sample_rate):
     T = n / sample_rate
     frq = k / T  # two sides frequency range
     frq = frq[:n // 2]  # one side frequency range
-
     Y = np.fft.fft(signal) / n  # fft computing and normalization
     Y = Y[:n // 2]
-
     plt.figure(figsize=(10, 6))
     plt.plot(frq, abs(Y))
     plt.xlabel('Frequency (Hz)')
     plt.ylabel('Amplitude')
     plt.title('Frequency Spectrum')
     plt.show()
+    plt.savefig("fft.png")
 
 def main():
     sample_rate = 1000  # Hz
     duration = 1  # seconds
     freq = 5  # Hz
-
     signal = generate_signal(freq, duration, sample_rate)
     plot_spectrum(signal, sample_rate)
 
 if __name__ == "__main__":
     main()
+
